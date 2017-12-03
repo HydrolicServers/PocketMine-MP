ebbee7207bda8773ad0f545a4202c7f441da460d
Mattemmodder <mathewpsutherland@gmail.com>
2017-12-03 13:01:49 +0000

    Update FoundersCommand.php

--- src/pocketmine/command/defaults/FoundersCommand.php
+++ src/pocketmine/command/defaults/FoundersCommand.php
@@ -27,11 +27,11 @@ class HelpCommand extends VanillaCommand{
 	public function __construct(string $name){
 		parent::__construct(
 			$name,
-			"%pocketmine.command.help.description",
-			"%commands.help.usage",
+			"%pocketmine.command.founders.description",
+			"%commands.founders.usage",
 			["?"]
 		);
-		$this->setPermission("pocketmine.command.help");
+		$this->setPermission("pocketmine.command.founders");
 	}
 	public function execute(CommandSender $sender, string $commandLabel, array $args){
 		if(!$this->testPermission($sender)){
@@ -75,77 +75,7 @@ class HelpCommand extends VanillaCommand{
 		}else{
 			if(($cmd = $sender->getServer()->getCommandMap()->getCommand(strtolower($command))) instanceof Command){
 				if($cmd->testPermissionSilent($sender)){
-					$message = TextFormat::YELLOW . "--------- " . TextFormat::WHITE . " Help: /" . $cmd->getName() . TextFormat::YELLOW . " ---------\n";
-					$message .= TextFormat::GOLD . "Description: " . TextFormat::WHITE . $cmd->getDescription() . "\n";
-					$message .= TextFormat::GOLD . "Usage: " . TextFormat::WHITE . implode("\n" . TextFormat::WHITE, explode("\n", $cmd->getUsage())) . "\n";
-					$sender->sendMessage($message);
-					return true;
-				}
-			}
-			$sender->sendMessage(TextFormat::RED . "No help for " . strtolower($command));
-			return true;
-		}
-	}
-}<?php
-/*
- *
- *
- * 
- *
- * @author HydrolicServers Team
- * 
- *
- *
-*/
-declare(strict_types=1);
-namespace pocketmine\command\defaults;
-use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
-use pocketmine\event\TranslationContainer;
-use pocketmine\utils\TextFormat;
-class HelpCommand extends VanillaCommand{
-	public function __construct(string $name){
-		parent::__construct(
-			$name,
-			"%pocketmine.command.founders.description",
-			"%commands.help.usage",
-			["?"]
-		);
-		$this->setPermission("pocketmine.command.founders");
-	}
-	public function execute(CommandSender $sender, string $commandLabel, array $args){
-		if(!$this->testPermission($sender)){
-			return true;
-		}
-		if(count($args) === 0){
-			$command = "";
-			$pageNumber = 1;
-		}elseif(is_numeric($args[count($args) - 1])){
-			$pageNumber = (int) array_pop($args);
-			if($pageNumber <= 0){
-				$pageNumber = 1;
-			}
-			$command = implode(" ", $args);
-		}else{
-			$command = implode(" ", $args);
-			$pageNumber = 1;
-		}
-		$pageHeight = $sender->getScreenLineHeight();
-		if($command === ""){
-			/** @var Command[][] $commands */
-			$commands = [];
-			foreach($sender->getServer()->getCommandMap()->getCommands() as $command){
-				if($command->testPermissionSilent($sender)){
-					$commands[$command->getName()] = $command;
-				}
-			}
-			ksort($commands, SORT_NATURAL | SORT_FLAG_CASE);
-			$commands = array_chunk($commands, $pageHeight);
-			$pageNumber = (int) min(count($commands), $pageNumber);
-			if($pageNumber < 1){
-				$pageNumber = 1;
-			}
-			$sender->sendMessage(TextFormat::RED . "No help for " . strtolower($command));
+			$sender->sendMessage(TextFormat::RED . "MrDevCat and Dogeee " . strtolower($command));
 			return true;
 		}
 	}
