f25d9ffe6433dc1c3f19a579e8c04a9e91f34cc0
Blazing Games <iiflamiinblaze123@gmail.com>
2017-12-07 15:47:12 -0500

    Its not pocket edition anymore :P

--- start.cmd
+++ start.cmd
@@ -1,5 +1,5 @@
 @echo off
-TITLE HydrolicServers server software for Minecraft: Pocket Edition
+TITLE HydrolicServers server software for Minecraft: Bedrock Edition
 cd /d %~dp0
 
 if exist bin\php\php.exe (
